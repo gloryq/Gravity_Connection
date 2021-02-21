2dda592f77c4deaae5acd9ffbc46fcd33d2101ad
Glory Quan <gloryquan2@gmail.com>
2021-02-20 21:24:43 -0800

    Allow gravity to be reversed when spacebar is pressed

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,16 +1,18 @@
 extends KinematicBody2D
 
+var velocity = Vector2()
+var gravity = 300
+var speed = 300
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("ui_space"):
+		gravity *= -1
+	
+	if gravity < 0:	
+		$"AnimatedSprite".flip_v = true
+	else:
+		$"AnimatedSprite".flip_v = false
+		
+	velocity.y = gravity
+	
+	velocity = move_and_slide(velocity)
