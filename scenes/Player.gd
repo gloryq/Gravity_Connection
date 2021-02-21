cb7e4614492387578e3c2328f8128d4de391a682
Glory Quan <gloryquan2@gmail.com>
2021-02-20 21:28:30 -0800

    Add left and right movement for testing

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,6 +5,13 @@ var gravity = 300
 var speed = 300
 
 func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	else:
+		velocity.x = 0
+		
 	if Input.is_action_just_pressed("ui_space"):
 		gravity *= -1
 	
