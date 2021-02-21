fd32be39e6ac710fcb1d9f7ce7e377b15b7a78e1
Perdyx <29049651+Perdyx@users.noreply.github.com>
2021-02-21 09:49:55 -0800

    Fix errors in level1_platforms

--- scenes/level1_platforms.gd
+++ scenes/level1_platforms.gd
@@ -4,8 +4,6 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-func _process(delta):
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
